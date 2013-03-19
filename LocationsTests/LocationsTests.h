fd1a280b85ef9046773b3db2cc30c566a479aec7
Администратор <administrator@localhost.(none)>
2013-03-20 00:27:56 +0500

    Initial Commit

---
+++ LocationsTests/LocationsTests.h
@@ -0,0 +1,13 @@
+//
+//  LocationsTests.h
+//  LocationsTests
+//
+//  Created by Администратор on 3/20/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LocationsTests : SenTestCase
+
+@end
---
+++ LocationsTests/LocationsTests.m
@@ -0,0 +1,32 @@
+//
+//  LocationsTests.m
+//  LocationsTests
+//
+//  Created by Администратор on 3/20/13.
+//  Copyright (c) 2013 Администратор. All rights reserved.
+//
+
+#import "LocationsTests.h"
+
+@implementation LocationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LocationsTests");
+}
+
+@end
